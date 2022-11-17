71790b37e2ba98ba9f1c038e5cccf22b58637181
Rahim Jamal <rahim_92@live.ca>
2022-11-17 16:02:25 -0500

    add database queries to functions

--- migrations/scheema.sql
+++ migrations/scheema.sql
@@ -9,11 +9,12 @@ CREATE TABLE users (
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
 );
+-- owner_id, title, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces,  number_of_bathrooms, number_of_bedrooms,country, street, city, province, post_code, active
 
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
+  
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-
   title VARCHAR(255) NOT NULL,
   description TEXT,
   thumbnail_photo_url VARCHAR(255) NOT NULL,
@@ -22,13 +23,11 @@ CREATE TABLE properties (
   parking_spaces INTEGER  NOT NULL DEFAULT 0,
   number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
   number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
-
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
   province VARCHAR(255) NOT NULL,
   post_code VARCHAR(255) NOT NULL,
-
   active BOOLEAN NOT NULL DEFAULT TRUE
 );
 
